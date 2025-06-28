0f510ed4f6cd41a21170c22b2a30cdd51b5f35fd
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-06-28 15:38:51 -0400

    deactivate on message

--- main.ts
+++ main.ts
@@ -11,74 +11,68 @@ const app = new App({
 const adminToken = Deno.env.get("SLACK_ADMIN_TOKEN") || "";
 const adminCookie = `d=${Deno.env.get("SLACK_ADMIN_COOKIE")}`;
 
-// Listens to incoming messages that contain "hello"
-app.message("hello", async ({ message, say }) => {
-});
+// Okay, I trust if you're reading this, you're not going to go and delete this regex pattern link that I'm probably never going to reference again and that I don't feel like storing the delete URL to.
+// https://regex101.com/r/mY8acm/3
+// https://regex101.com/DON'Tdelete/EaVW5aHHujMKRecKvbLLJ2ncV6GW27HxvVcZ
+app.message(
+  /^(?:hey <\!subteam\^S07TQBKCVL7>!\r?\n)?there's someone that needs to be deactivated:/i,
+  async ({ body, client }) => {
+    console.debug(body);
+    const channelId = body.event.channel;
 
-app.event("reaction_added", async ({ body, client }) => {
-  console.debug(body);
-  const channelId = body.event.item.channel;
-  console.log(
-    "Got reaction",
-    body.event.reaction,
-    "in",
-    body.event.item.channel,
-    "original message",
-    body.event.item.ts,
-  );
-
-  const result = await client.conversations.history({
-    channel: channelId,
-    latest: body.event.item.ts,
-    inclusive: true,
-    limit: 1,
-  });
-  const message = result.messages && result.messages.length > 0
-    ? result.messages[0]
-    : undefined;
-  if (message == undefined) {
-    console.error("couldn't get message");
-    return;
-  }
-  console.debug(message);
-
-  const matches = message.text?.match(/U[A-Z0-9]+/g);
-  if (matches?.length == 1) {
-    const userId = matches[0];
-    console.log(`Deactivating ${userId}`);
-
-    try {
-      const formData = new FormData();
-      formData.append("user", userId);
-      const headers = new Headers();
-
-      // Add the cookie to the headers
-      headers.append("Cookie", adminCookie);
-      headers.append(
-        "Authorization",
-        `Bearer ${adminToken}`,
-      );
-      const resp = await fetch(
-        "https://slack.com/api/users.admin.setInactive",
-        {
-          method: "POST",
-          body: formData,
-          headers,
-        },
-      );
-
-      console.log(
-        await resp.json(),
-      );
-    } catch (err) {
-      console.error(err);
+    const result = await client.conversations.history({
+      channel: channelId,
+      latest: body.event.ts,
+      inclusive: true,
+      limit: 1,
+    });
+    const message = result.messages && result.messages.length > 0
+      ? result.messages[0]
+      : undefined;
+    if (message == undefined) {
+      console.error("couldn't get message");
       return;
     }
-  } else {
-    console.error("Unable to get 1 user ID");
-    return;
+    console.debug(message);
+
+    const matches = message.text?.match(/U[A-Z0-9]+/g);
+    if (matches?.length == 1) {
+      const userId = matches[0];
+      console.info(`Deactivating ${userId}`);
+
+      try {
+        const formData = new FormData();
+        formData.append("user", userId);
+        const headers = new Headers();
+
+        // Add the cookie to the headers
+        headers.append("Cookie", adminCookie);
+        headers.append(
+          "Authorization",
+          `Bearer ${adminToken}`,
+        );
+        const resp = await fetch(
+          "https://slack.com/api/users.admin.setInactive",
+          {
+            method: "POST",
+            body: formData,
+            headers,
+          },
+        );
+        console.debug(
+          await resp.json(),
+        );
+        
+      } catch (err) {
+        console.error(err);
+        return;
+      }
+    } else {
+      console.error("Unable to get 1 user ID");
+      return;
+    }
   }
-});
+);
 
 (async () => {
   // Start your app
