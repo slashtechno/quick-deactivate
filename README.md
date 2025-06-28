93ec35e28b3a64e460518a4c9bf170f228e43b3a
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-06-28 15:53:56 -0400

    fix regex stuff and add checkmark reaction

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Quick deactivate  
-This is a Slack bot built for [Hack Club](https://hackclub.com), a nonprofit organization serving as both a community for high-school programmers, makers, and hackers, but also as a resource hub providing grants and incentives, such as free laptops, Flipper Zeros, and a lot [more](https://summer.hackclub.com/).  
-These programs are <=18 bt we get many adults or people who want to defraud these programs trying to joining. We quickly identify them, but deactivating manually can get difficult as we need to deactivate more and more accounts. This tool aims to automate that by allowing us to simply react to a message sent by our existing verification infrastructure to quickly confirm the deactivation of a user.
+# Quick deactivate
+
+This is a Slack bot built for [Hack Club](https://hackclub.com), a nonprofit
+organization serving as both a community for high-school programmers, makers,
+and hackers, but also as a resource hub providing grants and incentives, such as
+free laptops, Flipper Zeros, and a lot [more](https://summer.hackclub.com/).\
+These programs are <=18 bt we get many adults or people who want to defraud
+these programs trying to joining. We quickly identify them, but deactivating
+manually can get difficult as we need to deactivate more and more accounts. This
+tool aims to automate that by allowing us to simply react to a message sent by
+our existing verification infrastructure to quickly confirm the deactivation of
+a user.
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import pkg from "npm:@slack/bolt";
 const { App } = pkg;
-import { WebClient } from "npm:@slack/web-api";
 
 // Initializes your app with your bot token and app token
 const app = new App({
@@ -12,10 +11,10 @@ const adminToken = Deno.env.get("SLACK_ADMIN_TOKEN") || "";
 const adminCookie = `d=${Deno.env.get("SLACK_ADMIN_COOKIE")}`;
 
 // Okay, I trust if you're reading this, you're not going to go and delete this regex pattern link that I'm probably never going to reference again and that I don't feel like storing the delete URL to.
-// https://regex101.com/r/mY8acm/3
+// https://regex101.com/r/mY8acm/4
 // https://regex101.com/DON'Tdelete/EaVW5aHHujMKRecKvbLLJ2ncV6GW27HxvVcZ
 app.message(
-  /^(?:hey <\!subteam\^S07TQBKCVL7>!\r?\n)?there's someone that needs to be deactivated:/i,
+  /^(?:hey <\!subteam\^S07TQBKCVL7>!(\r\n|\r|\n))?there's someone that needs to be deactivated:/i,
   async ({ body, client }) => {
     console.debug(body);
     const channelId = body.event.channel;
@@ -36,7 +35,7 @@ app.message(
     console.debug(message);
 
     const matches = message.text?.match(/U[A-Z0-9]+/g);
-    if (matches?.length == 1) {
+    if (matches && matches.length >= 1) {
       const userId = matches[0];
       console.info(`Deactivating ${userId}`);
 
@@ -62,7 +61,15 @@ app.message(
         console.debug(
           await resp.json(),
         );
-        
+
+        // React with :white_check_mark:
+        client.reactions.add(
+          {
+            channel: channelId,
+            timestamp: body.event.ts,
+            name: "white_check_mark",
+          },
+        );
       } catch (err) {
         console.error(err);
         return;
@@ -71,8 +78,28 @@ app.message(
       console.error("Unable to get 1 user ID");
       return;
     }
-  }
+  },
 );
+
+// Inspect message... but better :)
+app.event("reaction_added", async ({ body, client }) => {
+  console.debug(body);
+    const channelId = body.event.item.channel;
+    const result = await client.conversations.history({
+    channel: channelId,
+    latest: body.event.item.ts,
+    inclusive: true,
+    limit: 1,
+  });
+  const message = result.messages && result.messages.length > 0
+    ? result.messages[0]
+    : undefined;
+  if (message == undefined) {
+    console.error("couldn't get message");
+    return;
+  }
+  console.debug(message);
+});
 
 (async () => {
   // Start your app
