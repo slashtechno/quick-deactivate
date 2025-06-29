8cbb7b38608115d744fc7abbb6db4e48b0d2a73f
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-06-29 16:24:33 -0400

    feat: retroactivre deactivations work, even with only an email

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { matchesGlob } from "node:path";
 import pkg from "npm:@slack/bolt";
 import { WebClient, WebClient as WebClientType } from "npm:@slack/web-api";
 const { App } = pkg;
@@ -59,64 +60,89 @@ interface parseAndDeactivateOptions extends deactivateOptions {
 }
 async function parseAndDeactivate(options: parseAndDeactivateOptions) {
   const matches = options.messageContent.match(/U[A-Z0-9]+/g);
+  let userId = "";
   if (matches && matches.length >= 1) {
-    const userId = matches[0];
-    console.info(`Deactivating ${userId}`);
-
+    userId = matches[0]; // Matches entire matched string, matches[1] matches the first group
+  } else {
     try {
-      const respJson = await deactivate(
-        {
-          userId,
-          adminCookie: options.adminCookie,
-          adminToken: options.adminToken,
-        },
-      );
-      if (!respJson.ok) {
-        console.error(`Failed to deactivate user ${userId}:`, respJson);
-        if (respJson.error === "user_not_found") {
-          console.debug(
-            `User ${userId} not found, original message: ${options.messageContent}`,
-          );
-        }
-        // try {
-        //   options.client.reactions.remove(
-        //     {
-        //       channel: options.channelId,
-        //       timestamp: options.messageTs,
-        //       name: "white_check_mark",
-        //     },
-        //   );
-        // } catch (_err) {
-        //   // no reaction present, most likely
-        // }
+      const emailRegex = /([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/;
+      const emailMatch = options.messageContent.match(emailRegex);
+      if (!emailMatch) {
+        console.error("No email found in message content", options.messageContent);
         return;
       }
-
-      // React with :white_check_mark if not already reacted
-      const reactions = await options.client.reactions.get({
-        channel: options.channelId,
-        timestamp: options.messageTs,
+      const email = emailMatch[1];
+      const userInfo = await options.client.users.lookupByEmail({
+        email: email,
       });
-      if (
-        reactions.message?.reactions &&
-        reactions.message.reactions.some((r) => r.name === "white_check_mark")
-      ) {
-        console.debug("Reaction already exists, skipping reaction addition.");
+      if (userInfo.user && userInfo.user.id) {
+        userId = userInfo.user.id;
+      } else {
+        console.error(
+          `No user found for email: ${options.messageContent}, userInfo: ${
+            JSON.stringify(userInfo)
+          }`,
+        );
         return;
       }
-      options.client.reactions.add(
-        {
-          channel: options.channelId,
-          timestamp: options.messageTs,
-          name: "white_check_mark",
-        },
-      );
-    } catch (err) {
-      console.error(err);
+    } catch (_err) {
+      console.error("Error looking up user by email:", options.messageContent);
       return;
     }
-  } else {
-    console.error("Unable to get 1 user ID");
+  }
+
+
+  console.info(`Deactivating ${userId}`);
+  try {
+    const respJson = await deactivate(
+      {
+        userId,
+        adminCookie: options.adminCookie,
+        adminToken: options.adminToken,
+      },
+    );
+    if (!respJson.ok) {
+      console.error(`Failed to deactivate user ${userId}:`, respJson);
+      if (respJson.error === "user_not_found") {
+        console.debug(
+          `User ${userId} not found, original message: ${options.messageContent}`,
+        );
+      }
+      // try {
+      //   options.client.reactions.remove(
+      //     {
+      //       channel: options.channelId,
+      //       timestamp: options.messageTs,
+      //       name: "white_check_mark",
+      //     },
+      //   );
+      // } catch (_err) {
+      //   // no reaction present, most likely
+      // }
+      return;
+    }
+
+    // React with :white_check_mark if not already reacted
+    const reactions = await options.client.reactions.get({
+      channel: options.channelId,
+      timestamp: options.messageTs,
+    });
+    if (
+      reactions.message?.reactions &&
+      reactions.message.reactions.some((r) => r.name === "white_check_mark")
+    ) {
+      console.debug("Reaction already exists, skipping reaction addition.");
+      return;
+    }
+    options.client.reactions.add(
+      {
+        channel: options.channelId,
+        timestamp: options.messageTs,
+        name: "white_check_mark",
+      },
+    );
+  } catch (err) {
+    console.error(err);
     return;
   }
 }
@@ -129,8 +155,8 @@ async function deactivateAllUnmarkedUsers(
   adminCookie: string,
 ) {
   const query =
-    `in:#verifications-deactivations after:2025-06-23 -is:thread -unknown`;
-    // `in:#verifications-deactivations after:2025-06-23 -is:thread`;
+    // `in:#verifications-deactivations after:2025-06-23 -is:thread -unknown`;
+  `in:#verifications-deactivations after:2025-06-01 -is:thread`;
   // const messagesWithoutCheck: { text?: string; subtype?: string; reactions?: { name: string }[]; ts?: string }[] = [];
   let continueSearching = true;
   let page = 1;
@@ -142,9 +168,18 @@ async function deactivateAllUnmarkedUsers(
     });
     if (result.messages) {
       const messages = result.messages.matches ?? [];
-      console.debug(`Found ${messages.length} messages`);
+      console.debug(`Found ${messages.length} messages`, result);
       for (const msg of messages) {
-        if (msg.text && msg.ts) {
+        if (msg.text == "") {
+          let combinedText = "";
+          for (const block of msg.blocks || []) {
+            if (block.text){
+              combinedText += (block.text.text + "\n");``
+            }
+          }
+          msg.text = combinedText.trim();
+        }
+        if (msg.ts && msg.username === "identi-tea") {
           const reactions = client.reactions.get({
             channel: channelId,
             timestamp: msg.ts || "",
@@ -153,7 +188,7 @@ async function deactivateAllUnmarkedUsers(
             r,
           ) => r.name === "white_check_mark");
           if (!hasCheck) {
-            console.debug(`Message without checkmark: ${msg.text}`);
+            // console.debug(`Message without checkmark: ${msg.text}`);
             // if (true) {
             await parseAndDeactivate(
               {
@@ -166,17 +201,25 @@ async function deactivateAllUnmarkedUsers(
                 adminCookie: adminCookie,
               },
             );
+          } else {
+            // console.debug(
+              // `Message with checkmark, skipping: ${msg.text}`,
+            // );
           }
+        } else{
+          console.debug(
+            `Skipping message without timestamp or not from identi-tea: ${msg.text}`,
+          );
         }
-        const currentPage = result.messages.pagination?.page || 1;
-        const totalPages = result.messages.pagination?.last || 1;
-        if (currentPage >= totalPages) {
-          continueSearching = false;
-          console.debug("No more pages to search.");
-        }
-        console.debug(`Last page: ${currentPage}, Total pages: ${totalPages}`);
-        page++;
       }
+      const currentPage = result.messages.pagination?.page || 1;
+      const totalPages = result.messages.pagination?.page_count || 1;
+      if (currentPage >= totalPages) {
+        continueSearching = false;
+        console.debug("No more pages to search.");
+      }
+      console.debug(`Last page: ${currentPage}, Total pages: ${totalPages}`);
+      page++;
     }
     // console.debug(`Found ${messagesWithoutCheck.length} messages without checkmark`);
     // return messagesWithoutCheck;
@@ -246,13 +289,13 @@ app.message(
 // });
 
 (async () => {
-  await deactivateAllUnmarkedUsers(
-    new WebClient(Deno.env.get("SLACK_BOT_TOKEN") || ""),
-    new WebClient(Deno.env.get("SLACK_USER_TOKEN") || ""),
-    LOG_CHANNEL,
-    adminToken,
-    adminCookie,
-  );
+  // await deactivateAllUnmarkedUsers(
+  //   new WebClient(Deno.env.get("SLACK_BOT_TOKEN") || ""),
+  //   new WebClient(Deno.env.get("SLACK_USER_TOKEN") || ""),
+  //   LOG_CHANNEL,
+  //   adminToken,
+  //   adminCookie,
+  // );
   // Start your app
   await app.start(Deno.env.get("PORT") || 3000);
 
