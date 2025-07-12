d5d497b46810103a54879c6742d268068c4b6800
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-07-11 20:39:31 -0400

    Add testing message to console logs

--- main.ts
+++ main.ts
@@ -400,4 +400,5 @@ app.command(
   await app.start(Deno.env.get("PORT") || 3000);
 
   app.logger.info("⚡️ Bolt app is running, or at least we can hope it is");
+  app.logger.info("If you're seeing this, you're probably in the right place");
 })();
